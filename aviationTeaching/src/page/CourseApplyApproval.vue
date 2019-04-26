96f82bc729d411842240fbd08a8acc6093e436e3
zhaohuai <aoaione@163.com>
2019-04-26 15:50:24 +0800

    代码提交

--- aviationTeaching/src/page/CourseApplyApproval.vue
+++ aviationTeaching/src/page/CourseApplyApproval.vue
@@ -1,58 +1,64 @@
 <template>
-    <div class="container">
-        <detail-title :title="title"></detail-title>
-        <div class="courseList_container">
-            <div class="select_bar ax_default">
-                <div class="bar_right">
-                    <span>课件分类:&nbsp;</span>
-                    <div id="trigger" class="select_wrap">
-                        <select onmousedown="javascript:return false;" class="select_common">
-                            <option>{{courseList}}</option>
-                        </select>
-                    </div>
-                </div>
-                <div id="trigger1" class="bar_right">
-                    <span>课件权限:&nbsp;</span>
-                    <select onmousedown="javascript:return false;" class="select_common">
-                        <option>{{applyStatus}}</option>
-                    </select>
-                </div>
-                <div class="search_bar">
-                    <input placeholder="请输入关键词,例如: 发布者、课件标题" class="search_input" v-model="keyword" />
-                    <a class="search_btn" @click="getCourseList()">搜索</a>
-                </div>
-            </div>
-            <div class="table_wrap ax_default">
-                <div class="table_title table_common">
-                    <span class="flex1">课件ID</span>
-                    <span class="flex2">课件标题</span>
-                    <span class="flex1">发布者</span>
-                    <span class="flex1">添加时间</span>
-                </div>
-                <div class="table_content table_common" v-for="course in courses" :key="course.id">
-                    <span class="flex1">
-                        <a class="linka" @click="toDetail(course.id)">{{course.id}}</a>
-                    </span>
-                    <span class="flex2">
-                        <a class="linka" @click="toDetail(course.id)">{{course.name}}</a>
-                    </span>
-                    <span class="flex1">
-                        {{course.publisher_name}}
-                    </span>
-                    <span class="flex1">
-                        {{course.add_time}}
-                    </span>
-                </div>
-            </div>
+  <div class="container">
+    <detail-title :title="title"></detail-title>
+    <div class="courseList_container">
+      <div class="select_bar ax_default">
+        <div class="bar_right">
+          <span>课件分类:&nbsp;</span>
+          <div id="trigger" class="select_wrap">
+            <select onmousedown="javascript:return false;" class="select_common">
+              <option>{{courseList}}</option>
+            </select>
+          </div>
         </div>
-        <bottom-tabbar :activeStatus="'course'"></bottom-tabbar>
+        <div id="trigger1" class="bar_right">
+          <span>课件权限:&nbsp;</span>
+          <select onmousedown="javascript:return false;" class="select_common">
+            <option>{{privilege}}</option>
+          </select>
+        </div>
+        <div class="search_bar">
+          <input placeholder="请输入关键词,例如: 发布者、课件标题" class="search_input" v-model="keyword" />
+          <a class="search_btn" @click="getCourseList()">搜索</a>
+        </div>
+      </div>
+      <div class="table_wrap ax_default">
+        <div class="table_title table_common">
+          <span class="flex1">课件ID</span>
+          <span class="flex2">课件标题</span>
+          <span class="flex1">申请状态</span>
+          <span class="flex1">申请人</span>
+        </div>
+        <div class="table_content table_common" v-for="course in courses" :key="course.id">
+          <span class="flex1">
+            <a class="linka" @click="toDetail(course.id)">{{course.id}}</a>
+          </span>
+          <span class="flex2">
+            <a class="linka" @click="toDetail(course.id)">{{course.name}}</a>
+          </span>
+          <span class="flex1" v-if="course.application_status==='apply_checking'" style="color:#FF9900;">
+            {{course.application_status|statusConver}}
+          </span>
+          <span class="flex1" v-else-if="course.application_status==='apply_refused'" style="color:#009966;">
+            {{course.application_status|statusConver}}
+          </span>
+          <span class="flex1" v-else>
+            {{course.application_status|statusConver}}
+          </span>
+          <span class="flex1">
+            {{course.application_user_name}}
+          </span>
+        </div>
+      </div>
     </div>
+    <bottom-tabbar :activeStatus="'course'"></bottom-tabbar>
+  </div>
 </template>
 <script>
 import MobileSelect from 'mobile-select'
 import detailTitle from '@/components/DetailTitle'
 import bottomTabbar from '@/components/BottomTabbar'
-import { list, courseTypeList } from '@/service/service'
+import { applyCourseList, courseTypeList } from '@/service/service'
 
 export default {
   name: 'CourseApplyApproval',
@@ -64,7 +70,7 @@ export default {
     return {
       title: '课件申请批复',
       courseList: '全部',
-      applyStatus: '全部',
+      privilege: '全部',
       courseLists: [],
       limits: [
         {id: '1', value: '申请中'},
@@ -77,7 +83,24 @@ export default {
       page_index: 1,
       page_count: 8,
       keyword: '',
-      privilege: 'all'
+      status: 'all'
+    }
+  },
+  filters: {
+    statusConver: function (value) {
+      if (!value) return ''
+      let newValue
+      switch (value) {
+        case 'apply_checking':
+          newValue = '申请中'
+          break
+        case 'apply_passed':
+          newValue = '已通过'
+          break
+        default:
+          newValue = '未通过'
+      }
+      return newValue
     }
   },
   watch: {
@@ -120,12 +143,12 @@ export default {
       let data = {
         'type_id': this.type_id,
         'privilege': this.privilege,
-        'status': 'all',
+        'status': this.status,
         'keyword': this.keyword,
         'page_index': this.page_index,
         'page_count': this.page_count
       }
-      list(data).then(res => {
+      applyCourseList(data).then(res => {
         this.courses = res.data.data
       })
     }
@@ -142,7 +165,17 @@ export default {
         {data: that.limits}
       ],
       callback: function (indexArr, data) {
-        that.applyStatus = data[0].value
+        switch (data[0].id) {
+          case '1':
+            that.status = 'applying'
+            break
+          case '2':
+            that.status = 'passed'
+            break
+          default:
+            that.status = 'refused'
+            break
+        }
         that.privilege = data[0].value
       },
       triggerDisplayData: false
