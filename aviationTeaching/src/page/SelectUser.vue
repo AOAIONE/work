fd79b30eefea89b7db9b105e4c9e48b1779c9ced
luoming <18618321192@163.com>
2019-04-28 14:33:21 +0800

    修改选择教员页面

--- aviationTeaching/src/page/SelectUser.vue
+++ aviationTeaching/src/page/SelectUser.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="container">
     <detail-title :title="title"></detail-title>
-    <div class="search_bar mar_top ax_default ">
-      <input placeholder="请输入关键词,例如: 教员ID、姓名、手机号" class="search_input" v-model="keyword" />
-      <a @click="serach()" class="search_btn ">搜索</a>
-    </div>
-    <div class="table_wrap ax_default ">
-      <div class="table_title table_common ">
-        <span class="flex1 ">教员ID</span>
-        <span class="flex1 ">姓名</span>
-        <span class="flex2 ">身份证</span>
-        <span class="flex1 ">课件分配</span>
-      </div>
-      <div class="table_content table_common " v-for="teacher in teachers " :key="teacher.user_id ">
-        <span class="flex1 ">
-          <a class="linka " @click="toDetail(teacher)">{{teacher.user_id}}</a>
-        </span>
-        <span class="flex1 ">
-          <a class="linka " @click="toDetail(teacher)">{{teacher.user_name}}</a>
-        </span>
-        <span class="flex2 ">
-          {{teacher.id_card_no}}
-        </span>
-        <span class="flex1 ">
-          <a v-if="teacher.is_designated===0 " class="course_tag1" @click="courseAssign(teacher.user_id)">课件分配</a>
-          <a v-else class="course_tag" @click="courseDelAssign(teacher.task_id)">解除分配</a>
-        </span>
-      </div>
-    </div>
-    <bottom-tabbar :activeStatus="'course'"></bottom-tabbar>
+     <div class="courseList_container">
+        <div class="select_bar ax_default pdt10">
+          <div class="search_bar">
+            <input placeholder="请输入关键词,例如: 教员ID、姓名、手机号" class="search_input" v-model="keyword" />
+            <a @click="serach()" class="search_btn ">搜索</a>
+          </div>
+        </div>
+        <div class="table_wrap ax_default ">
+          <div class="table_title table_common ">
+            <span class="flex1 ">教员ID</span>
+            <span class="flex1 ">姓名</span>
+            <span class="flex2 ">身份证</span>
+            <span class="flex1 ">课件分配</span>
+          </div>
+          <div class="table_content table_common " v-for="teacher in teachers " :key="teacher.user_id ">
+            <span class="flex1 ">
+              <a class="linka " @click="toDetail(teacher)">{{teacher.user_id}}</a>
+            </span>
+            <span class="flex1 ">
+              <a class="linka " @click="toDetail(teacher)">{{teacher.user_name}}</a>
+            </span>
+            <span class="flex2 ">
+              {{teacher.id_card_no}}
+            </span>
+            <span class="flex1 ">
+              <a v-if="teacher.is_designated===0 " class="course_tag1" @click="courseAssign(teacher.user_id)">课件分配</a>
+              <a v-else class="course_tag" @click="courseDelAssign(teacher.task_id)">解除分配</a>
+            </span>
+          </div>
+        </div>
+     </div>
+      <bottom-tabbar :activeStatus="'course'"></bottom-tabbar>
   </div>
 </template>
 <script>
