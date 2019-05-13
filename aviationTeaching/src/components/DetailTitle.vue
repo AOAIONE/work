38dd7b5c77bb51c7be6b931bb0ebbf0a7d9f8dbf
zhaohuai <aoaione@163.com>
2019-05-13 15:05:48 +0800

    bug更改

--- aviationTeaching/src/components/DetailTitle.vue
+++ aviationTeaching/src/components/DetailTitle.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="title_container ax_default">
     <p>
-      <a class="title_left" @click="back"><i class="iconfont">&#xe616;</i>返回</a>
+      <a v-if="!isHidden" class="title_left" @click="back">
+        <i class="iconfont">&#xe616;</i>返回
+      </a>
       <div class="title_center">{{title}}</div>
     </p>
   </div>
@@ -11,7 +13,7 @@
 
 export default {
   name: 'DetailTitle',
-  props: ['title'],
+  props: ['title', 'isHidden'],
   methods: {
     back: function () {
       this.$router.go(-1)
@@ -42,7 +44,7 @@ export default {
   font-size: 28px;
   font-family: "微软雅黑 Bold", "微软雅黑 Regular", "微软雅黑";
 }
-.title_left i{
+.title_left i {
   font-size: 30px;
   margin-right: 4px;
 }
